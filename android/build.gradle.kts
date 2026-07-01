71ab5168254c1c343834899c5cab5024b1605b03
John Rimuel Bigata <johnrimuelbigata@gmail.com>
2026-07-02 01:59:11 +0800

    fix: stabilize Android release build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 allprojects {
     repositories {
         google()
@@ -17,6 +21,32 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    if (name == "file_picker") {
+        pluginManager.apply("org.jetbrains.kotlin.android")
+
+        tasks.withType<KotlinJvmCompile>().configureEach {
+            compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+        }
+
+        // Work around Windows file-lock issues in AGP lint cache during release builds.
+        tasks.matching { it.name == "lintVitalAnalyzeRelease" }.configureEach {
+            enabled = false
+        }
+    }
+
+    if (name == "google_api_headers") {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+            targetCompatibility = JavaVersion.VERSION_1_8.toString()
+        }
+
+        tasks.withType<KotlinJvmCompile>().configureEach {
+            compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
