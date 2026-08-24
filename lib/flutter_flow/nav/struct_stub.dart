8ecd199e8e8fb7cec0413dcb7788463be71c836a
John Rimuel Bigata <johnrimuelbigata@gmail.com>
2026-08-24 22:28:18 +0800

    build(web): enable Flutter web preview + Netlify deploy config

---
+++ lib/flutter_flow/nav/struct_stub.dart
@@ -0,0 +1,6 @@
+/// Web stub for `dart:ffi`'s Struct, which does not exist on the web.
+/// Serialization only ever checks `param is Struct`, which is always
+/// false here since FFI types cannot exist on web.
+class Struct {
+  const Struct();
+}
